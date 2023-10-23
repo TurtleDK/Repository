082532d0828c187ade8562f53b669bf5cf5a5896
TurtleDK <98598321+TurtleDK@users.noreply.github.com>
2023-10-23 09:44:54 +0200

    Solution created

---
+++ EnKonsolApp/EnKonsolApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EnKonsolApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
